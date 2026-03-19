0db6728d5f34582e7d6c67104d9a8846dea7db5f
Вероника <zinurova-veronika@icloud.com>
2026-03-19 19:32:42 +0300

    базовые стили style.css и index.html главная стр

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/form')
+def form():
+    return "Страница с формой будет тут"
+
+if __name__ == '__main__':
+    app.run(debug=True)
